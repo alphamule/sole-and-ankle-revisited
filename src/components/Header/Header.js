173fe6d6b04dabae3340aa0b4c07404664f2eb72
Eric Wagner <github@alphamule.com>
2021-05-17 16:29:43 -0700

    module 2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import { COLORS, WEIGHTS, QUERIES } from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
+import Icon from '../Icon';
+import UnstyledButton from '../UnstyledButton';
+import VisuallyHidden from '../VisuallyHidden';
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -21,15 +24,30 @@ const Header = () => {
         <Side>
           <Logo />
         </Side>
-        <Nav>
+        <DesktopNav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
           <NavLink href="/men">Men</NavLink>
           <NavLink href="/women">Women</NavLink>
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
+        </DesktopNav>
+        <Side>
+          <MobileNav>
+            <UnstyledButton href="/sale">
+              <Icon id="shopping-bag" />
+              <VisuallyHidden>Open shopping cart</VisuallyHidden>
+            </UnstyledButton>
+            <UnstyledButton href="/find">
+              <Icon id="search" />
+              <VisuallyHidden>Open search</VisuallyHidden>
+            </UnstyledButton>
+            <UnstyledButton href="/menu">
+              <Icon id="menu" />
+              <VisuallyHidden>Open menu</VisuallyHidden>
+            </UnstyledButton>
+          </MobileNav>
+        </Side>
       </MainHeader>
 
       <MobileMenu
@@ -46,16 +64,48 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+
+  @media ${QUERIES.tabletAndSmaller} {
+    border-top: 4px solid ${COLORS.gray[900]};
+    justify-content: space-between;
+  }
+
+  @media ${QUERIES.phoneAndSmaller} {
+    padding: 16px;
+  }
 `;
 
-const Nav = styled.nav`
+const DesktopNav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
+`;
+
+const MobileNav = styled.div`
+  display: none;
+  gap: 32px;
+  margin: 0px;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: flex;
+  }
+
+  @media ${QUERIES.phoneAndSmaller} {
+    display: flex;
+    gap: 16px;
+  }
 `;
 
 const Side = styled.div`
   flex: 1;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    flex: unset;
+  }
 `;
 
 const NavLink = styled.a`
